e25c0fb4d25e320ed04b291bfd177c67beab37d1
mattols <mattolson616@gmail.com>
2018-04-09 16:05:30 -0600

    added more efficient method to use lookup table and code now runs the entire scene at one rather than row by row. This also fixes striping issue caused by row indexing

--- CASI_Grain_Retreival.R
+++ CASI_Grain_Retreival.R
@@ -12,17 +12,26 @@ require(caTools)
 
 setwd("~/data/ASO")
 
-### READ IN SNICAR DATA
+### DATA
+
+# READ IN SNICAR DATA
 tbl_snicar = read.csv("tables/LookupTable2.csv")
 tbl_snicar[,1] = tbl_snicar[,1]*1000
 
-### READ IN CASI DATA
-r = brick("CASI/class_image/CASI_2017_02_21_ort2_mosaic_specalb")
-# obtain a list of wavelengths
-aso_wv = as.numeric(unlist(lapply(strsplit(names(r),"\\.{3}"),
+# READ IN CASI DATA
+r = brick("CASI/20170220/174008/CASI_2017_02_20_174008_atm_ort")
+# List of wavelengths
+aso_wv = as.numeric(unlist(lapply(strsplit(names(r),"bsq\\.{3}"),
                                   function(x) substr(x[2],1,nchar(x[2])-1))))
-# create new dataframe with ASO wavelength properties
+# New dataframe with ASO wavelength properties
 aso_df = data.frame("center"=aso_wv*1000,'fwhm'=3.5)
+aso_wv = aso_wv * 1000 # convert to nm
+names(r) = aso_wv # rename
+
+# select bands used for integration
+stk = r[[c(62:72)]]
+
+### LOOKUP TABLE ###
 
 # SPECTRAL RESAMPLING (all at once)
 s_mat = speclib(as.matrix(tbl_snicar[,2:ncol(tbl_snicar)]),
@@ -39,10 +48,8 @@ lines((snicar_resample@wavelength),
       col='blue')
 abline(v=1030,col='red',lty=2)
 
-#### Integrated Depth
-# change dimentions first >> 
+#### Integrated Depth (NOT USED)
 ss <- snicar_resample[,c(62:72)] #tail(aso_df[,1],11) ???
-# Continuum
 
 # Compute endpoints for each grain size
 shortb = snicar_resample[c(1:1471),62]@spectra@spectra_ma
@@ -61,7 +68,7 @@ for(i in 1:11){
   cont[i,] = (i-1)*slope + shortb
 }
 
-#test ****
+#test ****PLOT
 plot(snicar_resample[200,c(62:72)],type='l')
 lines(snicar_resample@wavelength[62:72],cont[,200],col='red',lty=2)
 integrand[,200]
@@ -76,39 +83,29 @@ for (i in 1:11){
 integral = colSums(integrand)
 grain_size = seq(30,1500)
 lookup = data.frame(integral,grain_size)
-lookup2 <- rbind(c(0.08,NA),lookup) # anything too small (not likely snow)
-lookup2 <- rbind(lookup2, c(0.71,NA)) # or anything too large
+lookup2 <- rbind(c(0.09,NA),lookup) # anything too small (not likely snow)
+lookup2 <- rbind(lookup2, c(0.7,NA)) # or anything too large
 # arbitrary numbers chosen
-head(lookup)
+head(lookup2)
 
 # END RESULT == LOOKUP table
 
 ##########################################################################################################
 ## # # # # ### ### ## ## #   # # # # # # # # #
 # CREATE
-### READ IN CASI DATA
-r = brick("CASI/20170220/174008/CASI_2017_02_20_174008_atm_ort")
-# obtain a list of wavelengths
-aso_wv = as.numeric(unlist(lapply(strsplit(names(r),"bsq\\.{3}"),
-                                  function(x) substr(x[2],1,nchar(x[2])-1))))
-# create new dataframe with ASO wavelength properties
-aso_df = data.frame("center"=aso_wv*1000,'fwhm'=3.5)
-aso_wv = aso_wv * 1000 # convert to nm
-names(r) = aso_wv
 
-# casi_mat = speclib(r,
+### casi_mat = speclib(r,
 #                 aso_wv)
 ####
 #### Integrated Depth
 # CONVERT to spectral (only essential bands)
+#### casi_mat2 <- casi_mat[,c(62:72)] #tail(aso_df[,1],11) ???
 # casi_mat = speclib(spectra=brick(stk),wavelength=aso_wv[c(62:72)], fwhm=rep(3.5,length(aso_wv[c(62:72)])))
-# casi_mat2 <- casi_mat[,c(62:72)] #tail(aso_df[,1],11) ???
 
 # DO BY ROW
 #####################
 strt = Sys.time()
-gz <- stk[[1]]
-gz[] <- NA
+gz <- raster(r)
 
 for (j in 1:nrow(stk)){
   print(paste("...solving for column", j, "of", ncol(stk)))
@@ -168,15 +165,19 @@ hdr(r2, format="ENVI")
 mask1 = brick("CASI/20170220/174008/CASI_2017_02_20_174008_classified_ort")
 gz_mask <- raster::mask(gz, mask1, filename="", inverse=FALSE,
      maskvalue=c(0,2,3,4))
+r2 = gz_mask
+filename = "CASI_2017_02_20_174008_snow_grain_mask.grd"
+r2 <- writeRaster(r2, filename, overwrite=TRUE)
+hdr(r2, format="ENVI")
 
-# plot RGB and grain size
+# plot RGB and grain size (blue)
 par(mfrow=c(1,2))
 par(oma=c(0,0,0,0))
 plotRGB(r, r=23, g=19, b=8, stretch="lin")
 plot(gz_mask,col=blues9,add=T)
 plotRGB(r, r=23, g=19, b=8, stretch="lin")
 
-# plot RGB and grain size
+# plot RGB and grain size (rainbow)
 par(mfrow=c(1,2))
 par(oma=c(0,0,0,0))
 plotRGB(r, r=23, g=19, b=8, stretch="lin")
@@ -194,4 +195,89 @@ par(mar=c(4.5,4.5,2,0))
 hist(gz_mask[[1]], col='deepskyblue2',
      main= "CASI_2017_02_20_174008",
      xlab="Grain size (micro m)")
+
+
+
+# Show histogram distribution of grain size by aspect
+# show grain size vs. elevation also
+
+
+
+
+
+
+
+
+
+# stop() # NEXT IS IMPROVED METHOD
+###
+#
+#
+#
+####
+##########################################################
+### ALL AT ONCE (VERY FAST)
+
+# Transform list to raster
+make_raster <-function(list,reference_raster){
+  # Creates raster from a list 
+  raster(
+    matrix(list,nrow=nrow(reference_raster),
+           ncol=ncol(reference_raster), byrow=TRUE),
+    xmn=reference_raster@extent@xmin, xmx=reference_raster@extent@xmax,
+    ymn=reference_raster@extent@ymin, ymx=reference_raster@extent@ymax, 
+    crs=crs(reference_raster))
+}
+
+# Select bands in rasterbrick
+stk = r[[c(63:72)]]
+
+# Create grain size raster
+gz = raster(r)
+spec_vals <- stk[] 
+n_bands <- dim(spec_vals)[2]
+
+# Create continuum 
+shortb <- spec_vals[,1]
+longb <- spec_vals[,n_bands]
+slope = (longb - shortb)/(n_bands-1)
+cont = array(dim=c(n_bands, length(shortb)))
+for(i in 1:n_bands){
+  print(i)
+  cont[i,] = (i-1)*slope + shortb
+}
+
+# Create integrand
+integrand = array(dim=c(n_bands, length(shortb)))
+for (i in 1:n_bands){
+  integrand[i,] <- (cont[i,] - spec_vals[,i] )/cont[i,]
+} 
+
+# Lookup table
+strt = Sys.time()
+integrals = colSums(integrand)
+DT <- data.table(lookup2)
+setkey(DT,integral)
+dt_size <- DT[J(integrals), roll='nearest']
+gz <- make_raster(dt_size$grain_size,gz)
+print(Sys.time() - strt)
+##############################################################
+
+
+
+
+
+#TEST shape of modeled and measures spectral profiles
+# Snicar
+for (i in seq(1,1471,30)){
+  if (i == 1){
+    plot(snicar_resample[i][,c(62:72)],ylim=c(0.3,0.93))
+  } else{
+    lines((snicar_resample@wavelength[c(62:72)]),
+          (snicar_resample[i,]@spectra@spectra_ma[c(62:72)]),
+          col='black',lty=2)
+  }
+}
+# CASI???
+
 
